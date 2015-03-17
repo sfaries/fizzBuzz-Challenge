5e54f7557a2048c63c2b7fc4a83e4438e6a59f8c
sfaries <shaunfaries@gmail.com>
2015-03-17 17:35:18 -0400

    Initial commit

---
+++ fizzBuzz.js
@@ -0,0 +1,130 @@
+	
+/*
+	for (var i = 1; i < 101; i++) 
+		{
+			document.write("<br />");
+
+			if ((i % 3 == 0) && (i % 5 == 0)) {
+			document.write('fizzbuzz');
+			} 
+			else if ((i % 3 == 0) && (i % 5 != 0)) {
+			document.write('fizz'); 
+			} 
+			else if ((i % 5 == 0) && (i % 3 != 0)) {
+			document.write('buzz'); 
+			} 
+			else {
+			document.write(i); 
+			}
+		}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+var i = ()
+
+//This is the loop that will count from 1 to 100
+for (var i = 1; i < 101; i++) {
+	print(i);
+}
+
+//This is the conditional that will replace and print? fizz/buzz
+function assignTerm (i) {
+
+	if ((i % 3 == 0) && (i % 5 == 0)) {
+		return 'fizzbuzz';
+	} else if ((i % 3 == 0) && (i % 5 != 0)) {
+		return 'fizz';
+	} else if ((i % 5 == 0) && (i % 3 != 0)) {
+		return 'buzz';
+	} else {
+		return var i
+	}
+}
+
+//possible var to use
+/*
+var fizz = (i % 3 == 0)
+var buzz = (i % 5 == 0)
+var fizzbuzz = ((i % 3 == 0) && (i % 5 == 0))
+*/
+
+
+//http://www.sitepoint.com/jquery-vs-raw-javascript-1-dom-forms/
+/*
+var i = ()
+
+function assignTerm () {
+
+	for (var i = 1; i < 101; i++) {
+	print(i);
+	}
+
+	if ((i % 3 == 0) && (i % 5 == 0)) {
+		print('fizzbuzz');
+	} else if ((i % 3 == 0) && (i % 5 != 0)) {
+		print('fizz');
+	} else if ((i % 5 == 0) && (i % 3 != 0)) {
+		print('buzz');
+	} else {
+		print(i)
+	}
+}
+
+
+
+
+//I think this might be the fizzbuzz code block...
+	for (var i = 1; i < 101; i++) {
+	
+		if ((i % 3 == 0) && (i % 5 == 0)) {
+		document.write('fizzbuzz');
+		} else if ((i % 3 == 0) && (i % 5 != 0)) {
+		document.write('fizz'); 
+		} else if ((i % 5 == 0) && (i % 3 != 0)) {
+		document.write('buzz'); 
+		} else {
+		document.write(i); 
+		}
+	}
+
+
+
+
+
+
+
+	function assignTerm (i) {
+				for (var i = 1; i < 101; i++) {
+					if ((i % 3 == 0) && (i % 5 == 0)) {
+			return 'fizzbuzz';
+			} else if ((i % 3 == 0) && (i % 5 != 0)) {
+			return 'fizz';
+			} else if ((i % 5 == 0) && (i % 3 != 0)) {
+			return 'buzz';
+			} else {
+				return i;
+			}
+		}
+	}
+
+	//This block of code fucking loops!!!!
+
+
+			var i=(1);
+
+			for (var i = 1; i < 101; i++) 
+			{
+			document.write(i)
+			document.write("<br />");	
+	}
